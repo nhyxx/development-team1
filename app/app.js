72ec52f3e9bd38589a15b38aa9a2da61f5f18464
nhyxx <leehyeoilin@gmail.com>
2023-11-09 21:12:16 +0900

    MongoDB

---
+++ app/app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const nunjucks = require('nunjucks');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
+
+const connect = require('./schemas');
+const pageRouter = require('./routes/page');
+const authRouter = require('./routes/auth');
+const passportConfig = require('./passport');
+
+
+dotenv.config();
+const app = express();
+
+app.set('port', process.env.PORT || 3002);
+app.set('view engine', 'html');
+nunjucks.configure('views', {
+    express: app,
+    watch: true,
+});
+connect();
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+    reave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    }
+}))
+
+
+
+app.use((req, res, next) => {
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    error.status = 404;
+    next(error);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 대기 중');
+});
---
+++ script.js
@@ -0,0 +1,7 @@
+function newPage()  {
+    window.open('https://github.com/nhyxx');
+  }
+
+  function newPage()  {
+    window.open('https://blog.naver.com/nytrippim');
+  }
